b0abdbb5ca3066abbaa0225928a96def54a16d5e
Toni Fisler <mail@tonifisler.ch>
2014-11-25 00:45:56 +0100

    remove useless line

--- app/index.js
+++ app/index.js
@@ -5,9 +5,6 @@ var yeoman = require('yeoman-generator');
 var yosay = require('yosay');
 var chalk = require('chalk');
 
-
-util.inherits(Generator, yeoman.generators.NamedBase);
-
 var AppGenerator = yeoman.generators.Base.extend({
   init: function () {
     this.pkg = require('../package.json');
