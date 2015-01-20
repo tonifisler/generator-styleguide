5b240e37dd2ad3405c3f6ae95e82ec5732388bb9
Toni Fisler <mail@tonifisler.ch>
2015-01-20 18:41:55 +0100

    switch to gulp-sass

--- app/templates/gulpfile.js
+++ app/templates/gulpfile.js
@@ -88,24 +88,20 @@ gulp.task('img', function() {
  * Build styles from SCSS files
  * With error reporting on compiling (so that there's no crash)
  */
- gulp.task('styles', function() {
-   if (argv.production) { console.log('[styles] Processing styles for production env.' ); }
-   else { console.log('[styles] Processing styles for dev env. No minifying here, for sourcemaps!') }
+gulp.task('styles', function() {
+  if (argv.production) { console.log('[styles] Processing styles for production env.' ); }
+  else { console.log('[styles] Processing styles for dev env. No minifying here, for sourcemaps!') }
 
-   return $.rubySass('assets/sass/main.scss', {sourcemap: !argv.production})
-       .on('error', $.notify.onError(function (error) {
-          console.log('Error', error.message);
-          if (!argv.production) {
-            return 'Message to the notifier: ' + error.message;
-          }
-       }))
-     .pipe($.autoprefixer({
-       browsers: ['last 2 versions', 'safari 5', 'ie 8', 'ie 9', 'ff 27', 'opera 12.1']
-     }))
-     .pipe($.if(!argv.production, $.sourcemaps.write()))
-     .pipe($.if(argv.production, $.minifyCss()))
-     .pipe(gulp.dest('build/css'));
- });
+  return gulp.src('assets/sass/main.scss')
+    .pipe($.sass({errLogToConsole: true}))
+    .pipe($.if(!argv.production, $.sourcemaps.init()))
+    .pipe($.autoprefixer({
+      browsers: ['last 2 versions', 'safari 5', 'ie 8', 'ie 9', 'ff 27', 'opera 12.1']
+    }))
+    .pipe($.if(!argv.production, $.sourcemaps.write()))
+    .pipe($.if(argv.production, $.minifyCss()))
+    .pipe(gulp.dest('build/css'));
+});
 
 /**
  * Build JS
